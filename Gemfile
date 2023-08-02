c1a8e5faf5b6923864476307d47ebff24b03cc69
felixNyalenda <felixasejunior@gmail.com>
2023-08-02 15:09:04 +0300

    add exercise complete

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "3.2.2"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.6"
 
+gem 'activestorage'
 # Use postgresql as the database for Active Record
 gem "pg", "~> 1.1"
 
@@ -25,6 +26,9 @@ gem 'bcrypt'
 
 gem 'carrierwave'
 gem 'mini_magick'
+
+gem "cloudinary"
+
 
 
 group :development, :test do
--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,6 @@
 class ExercisesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_exercise_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
   def index
     exercises = Exercise.all
     render json: exercises, status: :ok
@@ -9,19 +10,24 @@ class ExercisesController < ApplicationController
     exercise = find_exercise
     render json: exercise, status: :ok
   end
-  # def create
-  #   exercise = Exercise.create!(exercise_params)
-  #   render json: exercise, status: :created
-  # end
+  def create
+    image = Cloudinary::Uploader.upload(params[:image])
+    exercise = Exercise.create(name:params[:name], description:params[:description], intensity_type:params[:intensity_type], image: image['url'])
+    render json: exercise, status: :created
+  end
 
   private
   def exercise_params
-    params.permit(:name, :description, :image, :intensity_type)
+    params.permit(:name, :description,:image, :intensity_type)
   end
   def find_exercise
     Exercise.find(params[:id])
   end
   def render_exercise_not_found
     render json: {error: "Exercise not found!"}, status: :not_found
+  end
+
+  def render_unprocessable_entity(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
