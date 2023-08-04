4598f08cc1028b87494475ab6b902495ed258125
felixNyalenda <felixasejunior@gmail.com>
2023-08-04 11:08:56 +0300

    --

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,9 +12,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
-
-    resource '/orders', # for specific restrictions(optional)
-             headers:any,
-             methods: [:post]
   end
 end
